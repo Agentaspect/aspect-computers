e26e48fb96514ee57f92f47c626e9561eb7d5d7e
agent aspect <agentaspect481@gmail.com>
2025-03-18 17:51:08 -0500

    header footer pages

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<footer>
+    <div class="outer bg-black">
+        <div class="inner spread-apart">
+            <a href="/"><img src="/logo-light.svg" alt="light logo"></a>
+            <a href="/blog" class="button bg-gradient-purple white"> Get Started</a>
+        </div>
+    </div>
+</footer>
+<style>
+    .inner {
+        padding-block: 1.5rem;
+    }
+</style>
